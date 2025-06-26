89ab792f7e0332a8085ce558dc09745e06a76627
dilip <dilipsalluri12@gmail.com>
2025-06-26 08:26:57 +0530

    Lab_001_HelloWorld

--- src/ex_04_Java_Basics_Strings/StringManipulationQ2.java
+++ src/ex_04_Java_Basics_Strings/StringManipulationQ2.java
@@ -1,7 +1,12 @@
 package ex_04_Java_Basics_Strings;
-
+//Program compares strings using various methods
 public class StringManipulationQ2 {
     public static void main(String[] args) {
-
+     String s1 = "Hello";
+     String s2 = "hello";
+     String s3 = "Hello";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s1.compareTo(s2));
     }
 }
